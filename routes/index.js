ac3622b9323a41d0a279dbb4d67d05e6a30d4d3f
Michael Lackey <michael.g.lackey@gmail.com>
2020-04-23 00:35:14 -0500

    change title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'My Dev Skills Express Lab' });
+  res.render('index', { title: 'Dev Skills Tracker' });
 });
 
 module.exports = router;
